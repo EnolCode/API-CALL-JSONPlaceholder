253deda9c4d7132bc9ce94246ceb4fc0fb681a16
Enol-Igareta <enol1code@gmail.com>
2023-01-11 08:18:06 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Api-CALL-JSONPlaceholder/", 
+  base: "/API-CALL-JSONPlaceholder/", 
   plugins: [vue()],
   resolve: {
     alias: {
